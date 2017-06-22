9d8f5447da2874586db48e9061acafb89248544c
Zach Hall <zach@zachhall.org>
2017-06-22 17:20:26 -0400

    Doc site font and spacing improvements

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ export class App {
   router: Router;
 
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Starboard';
+    config.title = 'ShadeCycle';
 
     config.map([
-      {route: '', moduleId: 'pages/home/home', title: 'Home'}
+      {route: '', moduleId: 'pages/home/home', title: 'Gradient text shadows in SASS!'}
     ]);
   }
 }
